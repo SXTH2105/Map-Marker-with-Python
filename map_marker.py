62823f2bd0568657609892c584af10008f0aa89d
Seth <sokpisethkong68@gmail.com>
2026-04-06 16:49:39 +0700

    Testing

--- map_marker.py
+++ map_marker.py
@@ -49,7 +49,8 @@ while True:
 
     m = folium.Map(location=[lat, lon], zoom_start=13)
 
-    folium.Marker([lat, lon], popup=location_name, tooltip=location_name, icon=folium.Icon(color="red", icon="info-sign")).add_to(m)
+    popup_text = f"<b>{location_name}</b><br>Lat: {lat:.6f}<br>Lon: {lon:.6f}"
+    folium.Marker([lat, lon], popup=popup_text, tooltip=location_name, icon=folium.Icon(color="red", icon="info-sign")).add_to(m)
 
     output_dir = "Saved Map"
     os.makedirs(output_dir, exist_ok=True)
